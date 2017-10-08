8699293b83d5ab9772a53ea34c9517085b088246
Joshua Shireman <kealist@gmail.com>
2017-10-08 13:27:21 -0400

    Boilerplate code

---
+++ JwtWebAPITemplate/JwtWebAPITemplate/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace JwtWebAPITemplate
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
