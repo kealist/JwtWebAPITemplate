98278e4b40c546932bd28ae03991a41f92172cd3
Joshua Shireman <kealist@gmail.com>
2017-10-08 15:02:36 -0400

    Implement ITokenModel interface using Jose

--- JwtWebAPITemplate/JwtWebAPITemplate/AuthorizationModels/TokenModel.cs
+++ JwtWebAPITemplate/JwtWebAPITemplate/AuthorizationModels/TokenModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Jose;
+using JwtWebAPITemplate.DataAccess;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,14 +10,34 @@ namespace JwtWebAPITemplate.AuthorizationModels
 {
     public class TokenModel : ITokenModel
     {
+        //Todo Add fields as desired for claims to verify
+
+        //For example:
+
+
         public string Generate(byte[] secret, Dictionary<string, Object> claims)
         {
-            throw new NotImplementedException();
+            return Jose.JWT.Encode(claims, secret, JwsAlgorithm.HS256);
         }
 
         public ApplicationUser VerifyToken(ApplicationUserManager userManager, byte[] secret, string jwt)
         {
-            throw new NotImplementedException();
+            try
+            {
+                //Make sure token is valid
+                string jsonString = JWT.Decode(jwt, secret, JwsAlgorithm.HS256);
+                var jwtToken = JsonConvert.DeserializeObject<TokenModel>(jsonString);
+                ApplicationUser user;
+
+                //Todo: Verify claims as desired.  Delete the line below and find the user in your database as needed
+                user = null;
+
+                return user;
+            }
+            catch //Todo: Specify exception types
+            {
+                return null;
+            }
         }
     }
 }
